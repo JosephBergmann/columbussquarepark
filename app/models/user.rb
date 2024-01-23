471e5cdb1075fbaacbe9ab67a8785bb6335ba019
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-01-23 14:54:22 -0500

    images model

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,8 @@ class User < ApplicationRecord
 
     has_secure_password
 
+    has_many :images
+
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
         user&.authenticate(password)
