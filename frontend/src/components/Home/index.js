fbc426b74d693f006c442169e42c912a5a4c3201
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2024-01-24 12:02:06 -0500

    adjusted icon sizes

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -6,7 +6,8 @@ import GalleryPreview from './GalleryPreview'
 
 export default function Home() {
 
-    const subHeaderClass = 'text-align-center tracking-widest xxs:text-lg xs:text-lg sm:text-xl md:text-2xl lg:w-3xl xl:4xl'
+    const subHeaderClass = 'text-center tracking-widest xxs:text-md xs:text-lg sm:text-xl md:text-2xl lg:w-3xl xl:4xl'
+    const iconClass = "sm:w-16 md:w-20 lg:w-24 xl:w-32"
 
     return (
         <>
@@ -15,20 +16,20 @@ export default function Home() {
             <div className='flex items-center justify-around gap-20 my-10'>
                 <div className='flex flex-col items-center'>
                     <span className={subHeaderClass} >Learn about the park</span>
-                    <img className="sm:w-10 md:w-16 lg:w-20 xl:w-32" src={lightbuld}/>
+                    <img className={iconClass} src={lightbuld}/>
                 </div>
                 <div className='flex flex-col items-center'>
                     <span className={subHeaderClass} >How to help</span>
-                    <img className="sm:w-10 md:w-16 lg:w-20 xl:w-32" src={shovel}/>
+                    <img className={iconClass} src={shovel}/>
                 </div>
                 <div className='flex flex-col items-center'>
                     <span className={subHeaderClass} >Upcoming Events</span>
-                    <img className="sm:w-10 md:w-16 lg:w-20 xl:w-32" src={calendar}/>
+                    <img className={iconClass} src={calendar}/>
                 </div>
             </div>
-            <div className='flex flex-col gap-4 mx-6 items-center'>
+            <div className='flex flex-col gap-4 mx-12 items-center'>
                 <span className={subHeaderClass}>View our gallery</span>
-                <div className='flex justify-around gap-6'>
+                <div className='flex justify-around gap-4'>
                     <GalleryPreview />
                 </div>
             </div>
--- frontend/src/components/ImageCard/index.js
+++ frontend/src/components/ImageCard/index.js
@@ -5,7 +5,7 @@ export default function ImageCard({image}) {
 
     return (
         <>
-            <img className='xxs:min-w-5 xs:min-w-10 sm:min-w-20 md:w-36 lg:w-52 xl:min-w-70 rounded-md' src={image}/>
+            <img className='xxs:max-w-5 xs:min-w-10 sm:min-w-20 md:w-36 lg:w-52 xl:min-w-70 rounded-sm' src={image}/>
         </>
     )
 }
