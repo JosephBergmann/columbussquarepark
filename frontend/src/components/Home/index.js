6f2600c669c9cbe67c435a9529ac03a87754e72f
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2024-01-23 22:36:41 -0500

    basic layout style for back ground image and icons below

--- frontend/src/components/Home/index.js
+++ frontend/src/components/Home/index.js
@@ -1,4 +1,4 @@
-import parkPhoto from './images/place_holder_park.jpeg'
+import parkPhoto from './images/temp_park.jpeg'
 import lightbuld from './images/lightbuld.jpeg'
 import shovel from './images/shovel_icon.jpeg'
 import calendar from './images/calendar_icon.jpeg'
@@ -8,24 +8,23 @@ export default function Home() {
 
     return (
         <>
-            <div>
-                <img src={parkPhoto}/>
+            <div class="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${parkPhoto})` }}>
             </div>
-            <div>
-                <div>
+            <div className='flex items-center justify-around gap-20 my-10'>
+                <div className='flex flex-col items-center'>
                     <span>Learn about the park</span>
-                    <img src={lightbuld}/>
+                    <img className="sm:w-20 md:w-36 lg:w-52 xl:w-70" src={lightbuld}/>
                 </div>
-                <div>
+                <div className='flex flex-col items-center'>
                     <span>How to help</span>
-                    <img src={shovel}/>
+                    <img className="sm:w-20 md:w-36 lg:w-52 xl:w-70" src={shovel}/>
                 </div>
-                <div>
+                <div className='flex flex-col items-center'>
                     <span>Upcoming Events</span>
-                    <img src={calendar}/>
+                    <img className="sm:w-20 md:w-36 lg:w-52 xl:w-70" src={calendar}/>
                 </div>
             </div>
-            <div>
+            <div className=''>
                 <span>View our gallery</span>
                 <GalleryPreview />
             </div>
