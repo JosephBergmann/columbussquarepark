ff1d33528db64d25ab547878dbe5cdc8f94defa8
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2024-01-31 18:15:41 -0500

    testing out react social media embed package

--- frontend/src/components/ImageCard/index.js
+++ frontend/src/components/ImageCard/index.js
@@ -1,11 +1,12 @@
-
+import { InstagramEmbed } from 'react-social-media-embed'
 
 
 export default function ImageCard({image}) {
 
     return (
         <>
-            <img className='xxs:max-w-5 xs:min-w-10 sm:min-w-20 md:w-36 lg:w-52 xl:min-w-70 rounded-sm' src={image}/>
+            {/* <img className='xxs:max-w-5 xs:min-w-10 sm:min-w-20 md:w-36 lg:w-52 xl:min-w-70 rounded-sm' src={image}/> */}
+            <InstagramEmbed url="https://www.instagram.com/p/C2SVvKeOzaw/" width={200} height{200}/>
         </>
     )
 }
