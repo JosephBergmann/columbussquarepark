fe9693b8c0b4e56e19b1e030838d52b569e614bd
Joseph Bergmann <joseph.g.bergmann@gmail.com>
2024-02-01 22:11:26 -0500

    began work on alldata

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,12 @@
 class StaticPagesController < ActionController::Base
     def frontend_index
+    debugger
       render file: Rails.root.join('public', 'index.html')
+    end
+
+    def get_all_data
+        debugger
+        @data = {events: Event.all}
+        render :all_data
     end
 end
