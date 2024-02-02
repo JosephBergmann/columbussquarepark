d62d29d39b237e52b53ca61c859526e03f157adb
Josh Goldenberg <129535504+jgoldenberg29@users.noreply.github.com>
2024-02-01 21:24:51 -0500

    home page adaptable to all screen sizes

--- frontend/src/components/ImageCard/index.js
+++ frontend/src/components/ImageCard/index.js
@@ -5,7 +5,7 @@ export default function ImageCard({image}) {
 
     return (
         <>
-            <img className='xxs:max-w-5 xs:min-w-10 sm:min-w-20 md:w-36 lg:w-52 xl:min-w-70 rounded-sm' src={image}/>
+            <img className='mind-w-16 sm:max-w-28 md:max-w-36 lg:min-w-48 xl:max-w-70 rounded-sm' src={image}/>
         </>
     )
 }
